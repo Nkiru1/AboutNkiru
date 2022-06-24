1d78140f8a949f3971fe2f1ee3bd82f2ea66d897
Nkiru <nkirusidi@gmail.com>
2022-06-24 13:08:36 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let myName = "UNkiru Okeke";
+let myHeight = "1.56m";
+let myCountry = "Nigeria";
+alert(
+  "My name is " +
+    myName +
+    "\n" +
+    "My height is " +
+    myHeight +
+    "\n" +
+    "My Country is " +
+    myCountry
+);
